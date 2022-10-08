d55a6c680c3607a459d7aa981e47529b0e775863
Rajajr <rajjr011@gmail.com>
2022-10-08 20:10:45 +0700

    UC-1 Create a Simple Linked List of 56,30 and 70

---
+++ LinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList
+    {
+        public Node Head;
+
+        public void AddLast(int data)
+        {
+            Node node1 = new Node(data);
+            if(this.Head==null)
+            {
+                this.Head = node1;
+                
+            }
+            else
+            {
+                Node temp = Head;
+                while(temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node1;    
+            }
+            Console.WriteLine("New Node Added in LinkedList: " +node1.data);
+            Console.WriteLine("--------------------------------------------");
+        }  
+        
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC-1 Create a Simple Linked List of 56,30 and 70
+            Console.WriteLine("welcome to LinkedList");
+
+            LinkedList linkedlist = new LinkedList();
+            linkedlist.AddLast(56);
+            linkedlist.AddLast(30);
+            linkedlist.AddLast(70);
+
+            
+           Console.ReadLine();
+        }
+    }
+}
